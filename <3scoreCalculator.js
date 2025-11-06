301ae40bad7aafc1a502aa5260a50ad07ab776b9
Ayesha Simran <khanayeshasimran@gmail.com>
2025-11-06 12:26:11 +0530

    love score calculator functionality

---
+++ <3scoreCalculator.js
@@ -0,0 +1,6 @@
+var person1 = prompt("Enter Name of person 1");
+var person2 = prompt("Enter Name of person 2");
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n)+1;
+alert("love between " + person1+ " and " + person2 + " is " + n);
